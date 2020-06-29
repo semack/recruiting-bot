f11a523ea6d5753e49874427a7ee456ce755cb19
Andriy S'omak <semack@gmail.com>
2020-06-29 22:04:13 +0300

    Added base turn to make bot alive after start

--- Dialogs/MainDialog.cs
+++ Dialogs/MainDialog.cs
@@ -1,6 +1,9 @@
-using Beetroot.RecruitingBot.Scrapers;
+using System.Threading;
+using System.Threading.Tasks;
 using Beetroot.RecruitingBot.Scrapers.Abstractions;
+using Microsoft.Bot.Builder;
 using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Schema;
 using Microsoft.Extensions.Logging;
 
 namespace Beetroot.RecruitingBot.Dialogs
@@ -11,6 +14,21 @@ namespace Beetroot.RecruitingBot.Dialogs
             : base(nameof(MainDialog))
         {
             var cities = scrapper.GetCitiesHaveVacanciesAsync().Result;
+            Dialogs.Add(new TextPrompt(nameof(TextPrompt)));
+            
+            var waterfallSteps = new WaterfallStep[]
+            {
+                IntroStepAsync,
+            };
+            AddDialog(new WaterfallDialog(nameof(WaterfallDialog), waterfallSteps));
+            InitialDialogId = nameof(WaterfallDialog);
+        }
+
+        private async Task<DialogTurnResult> IntroStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+            var messageText = stepContext.Options?.ToString() ?? "What can I help you with today?\nSay something like \"Book a flight from Paris to Berlin on March 22, 2020\"";
+            var promptMessage = MessageFactory.Text(messageText, messageText, InputHints.ExpectingInput);
+            return await stepContext.PromptAsync(nameof(TextPrompt), new PromptOptions { Prompt = promptMessage }, cancellationToken);
         }
     }
 }
